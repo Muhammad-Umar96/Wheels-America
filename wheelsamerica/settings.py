ff8fe5aca60e76a41599c3a32a3d7e373fd00fc7
Umar <mumarmunir96@gmail.com>
2026-02-03 18:17:15 +0500

    final

--- wheelsamerica/settings.py
+++ wheelsamerica/settings.py
@@ -14,89 +14,42 @@ NEWSPIDER_MODULE = "wheelsamerica.spiders"
 
 ADDONS = {}
 
-
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = "wheelsamerica (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
-# Concurrency and throttling settings
-#CONCURRENT_REQUESTS = 16
 
-DOWNLOAD_DELAY = 0
-RANDOMIZE_DOWNLOAD_DELAY = True
-
-HTTPCACHE_ENABLED = True
-HTTPCACHE_DIR = 'httpcache'
-HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
-HTTPCACHE_POLICY = 'scrapy.extensions.httpcache.DummyPolicy'
-
-LOG_LEVEL = 'INFO'
-
+# Downloader Middlewares
 DOWNLOADER_MIDDLEWARES = {
     "wheelsamerica.middlewares.RotateAgentMiddleware": 543,
 }
 
+CONCURRENT_REQUESTS = 8  # Much lower concurrency
+DOWNLOAD_DELAY = 1.0     # Wait 1 second between requests
+RANDOMIZE_DOWNLOAD_DELAY = True  # Randomize delay (0.5-1.5x)
+
+# Retry settings
 RETRY_TIMES = 5
 RETRY_HTTP_CODES = [500, 502, 503, 504, 408, 429]
 
-# Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+# Enable and configure the AutoThrottle extension
+# AUTOTHROTTLE_ENABLED = True
+# AUTOTHROTTLE_START_DELAY = 2
+# AUTOTHROTTLE_MAX_DELAY = 60
+# AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# AUTOTHROTTLE_DEBUG = False
 
-# Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
+# HTTP Caching
+HTTPCACHE_ENABLED = False
+HTTPCACHE_DIR = 'httpcache'
+HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+HTTPCACHE_POLICY = 'scrapy.extensions.httpcache.DummyPolicy'
 
-# Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
-#    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-#    "Accept-Language": "en",
-#}
+# Logging
+# LOG_LEVEL = 'INFO'
+LOG_FILE='spider.log'
 
-# Enable or disable spider middlewares
-# See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    "wheelsamerica.middlewares.WheelsamericaSpiderMiddleware": 543,
-#}
-
-# Enable or disable downloader middlewares
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    "wheelsamerica.middlewares.WheelsamericaDownloaderMiddleware": 543,
-#}
-
-# Enable or disable extensions
-# See https://docs.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
-#    "scrapy.extensions.telnet.TelnetConsole": None,
-#}
-
-# Configure item pipelines
-# See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "wheelsamerica.pipelines.WheelsamericaPipeline": 300,
-#}
-
-# Enable and configure the AutoThrottle extension (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
-# The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
-# The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
-# The average number of requests Scrapy should be sending in parallel to
-# each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
-# Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
-
-# Enable and configure HTTP caching (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
-#HTTPCACHE_EXPIRATION_SECS = 0
-#HTTPCACHE_DIR = "httpcache"
-#HTTPCACHE_IGNORE_HTTP_CODES = []
-#HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
-
-# Set settings whose default value is deprecated to a future-proof value
+# Feed settings
 FEED_EXPORT_ENCODING = "utf-8-sig"
--- wheelsamerica/spiders/wheels_spider.py
+++ wheelsamerica/spiders/wheels_spider.py
@@ -5,84 +5,85 @@ from wheelsamerica.items import WheelsamericaItem
 class WheelsSpiderSpider(scrapy.Spider):
     name = "wheels_spider"
     url = "https://wheelsamerica.com/shop/"
-    
+
     def start_requests(self):
-        yield scrapy.Request(url=self.url, callback=self.parse, meta={'page_num': 1})
+        yield scrapy.Request(
+            url='https://wheelsamerica.com/',
+            callback=self.parse,
+            dont_filter=True
+        )
+
 
     def parse(self, response):
-        page_num = response.meta.get('page_num', 1)
-        self.logger.info(f"Scraping page {page_num}")
+        self.logger.info(f"Scraping page: {response.url}")
         
-        wheels = response.css('div.product-small.box')
-        for wheel in wheels:
-            info = wheel.css('div.image-fade_in_back a::attr(href)').get()
-            yield scrapy.Request(url=info, callback=self.parse_wheel_details, meta={'page_num': page_num})
+        product_links = response.css('div.product-small.box div.box-text a.woocommerce-LoopProduct-link::attr(href)').getall()
+        if not product_links:
+            product_links = response.css('p.name.product-title a::attr(href)').getall()
+        
+        self.logger.info(f"Found {len(product_links)} products on this page")
+        
+        for link in product_links:
+            yield response.follow(link, self.parse_wheel_details)
 
-        # Move pagination logic here instead of in parse_wheel_details
-        if page_num < 693:
-            next_page_num = page_num + 1
-            next_page = f"https://wheelsamerica.com/shop/page/{next_page_num}/"
-            yield scrapy.Request(url=next_page, callback=self.parse, meta={'page_num': next_page_num})
+        next_page = response.css('ul.page-numbers a.next::attr(href)').get() 
+        if next_page:
+            self.logger.info(f"Following next page: {next_page}")
+            yield response.follow(next_page, self.parse)
 
     def parse_wheel_details(self, response):
-        page_num = response.meta.get('page_num', 'unknown')
-        self.logger.info(f"Parsing wheel detail from page {page_num}")
+        self.logger.info(f"Parsing wheel detail - URL: {response.url}")
         
         item = WheelsamericaItem()
+        
         title = response.css('h1.product-title.product_title.entry-title::text').get()
-        main_title = title.strip()
+        item['title'] = title.strip() if title else None
+        
         inventory = response.css('p.stock.in-stock::text').get()
-        if inventory:
-            num_inventory = inventory.split(' ')[0] 
-        else:
-            num_inventory = None  
+        item['inventory'] = inventory.split(' ')[0] if inventory else None
                  
         condition = response.css('div.wheel-specs p:nth-child(1) strong::text').get()
-        if condition:
-            fin_condition = condition.split(' ')[-1]
-        else:
-            fin_condition = None
+        item['condition'] = condition.split(' ')[-1] if condition else None
 
         int_vari = response.css('div.wheel-specs p:nth-child(2) strong::text').get()
         if int_vari:
-            interchange = int_vari.split('||')[0].split(':')[-1].strip()
-            variations = int_vari.split('||')[-1].split(':')[-1].strip()
+            parts = int_vari.split('||')
+            item['interchange'] = parts[0].split(':')[-1].strip() if ':' in parts[0] else parts[0].strip()
+            item['variations'] = parts[-1].split(':')[-1].strip() if len(parts) > 1 and ':' in parts[-1] else None
         else:
-            interchange = None
-            variations = None
+            item['interchange'] = None
+            item['variations'] = None
 
-        table = response.css('table.wheel-spec-table tbody tr')
-        for row in table:
-            td = row.css('td')
-            size = td[0].css('::text').get()
-            width = td[1].css('::text').get()
-            lugs = td[2].css('::text').get()
-            spokes = td[3].css('::text').get()
-            bolt_circle = td[4].css('::text').get()
-            finish = td[5].css('::text').get()
-        price = "".join(response.css('span.woocommerce-Price-amount.amount bdi ::text').getall()).strip()
-        desc2 = response.css('div.panel.entry-content p:nth-of-type(2)::text').get()
-        Int2 = response.css('div.panel.entry-content h5::text').get()
-        Int2_final = Int2.split(':')[-1].strip()
-        vehicles = response.css('div.panel.entry-content p:nth-of-type(4) i::text').getall()
-        partnumbers = response.css('div.panel.entry-content p:nth-of-type(5) i::text').getall()
-        idents = response.css('div.panel.entry-content p:nth-of-type(6) i::text').getall()
-
-        item['title'] = main_title
-        item['inventory'] = num_inventory
-        item['condition'] = fin_condition
-        item['interchange'] = interchange
-        item['variations'] = variations
+        size = width = lugs = spokes = bolt_circle = finish = None
+        
+        table_rows = response.css('table.wheel-spec-table tbody tr')
+        for row in table_rows:
+            tds = row.css('td')
+            if len(tds) >= 6:
+                size = tds[0].css('::text').get()
+                width = tds[1].css('::text').get()
+                lugs = tds[2].css('::text').get()
+                spokes = tds[3].css('::text').get()
+                bolt_circle = tds[4].css('::text').get()
+                finish = tds[5].css('::text').get()
+        
         item['sizes'] = size
         item['width'] = width
         item['lugs'] = lugs
         item['spokes'] = spokes
         item['bolt_circle'] = bolt_circle
         item['finish'] = finish
-        item['price'] = price
-        item['desc2'] = desc2
-        item['Int2'] = Int2_final
-        item['vehicles'] = vehicles
-        item['partnumbers'] = partnumbers
-        item['indents'] = idents
+        
+        price = "".join(response.css('div.product-price-container p.price.product-page-price span.woocommerce-Price-amount.amount bdi ::text').getall()).strip()
+        item['price'] = price if price else None
+        
+        item['desc2'] = response.css('div.panel.entry-content p:nth-of-type(2)::text').get()
+        
+        int2 = response.css('div.panel.entry-content h5::text').get()
+        item['Int2'] = int2.split(':')[-1].strip() if int2 and ':' in int2 else int2
+        
+        item['vehicles'] = response.css('div.panel.entry-content p:nth-of-type(4) i::text').getall()
+        item['partnumbers'] = response.css('div.panel.entry-content p:nth-of-type(5) i::text').getall()
+        item['indents'] = response.css('div.panel.entry-content p:nth-of-type(6) i::text').getall()
+        
         yield item
